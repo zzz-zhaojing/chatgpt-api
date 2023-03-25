28902e951925bae9d35e9854d1f473fb57232e5a
jasminezhao324 <52599324+jasminezhao324@users.noreply.github.com>
2023-03-25 16:38:21 +0800

    feat:api

--- chatbot-api-interfaces/src/test/java/cn/zj/chatbot/api/test/ApiTest.java
+++ chatbot-api-interfaces/src/test/java/cn/zj/chatbot/api/test/ApiTest.java
@@ -1,7 +1,20 @@
 package cn.zj.chatbot.api.test;
 
+import org.junit.Test;
+
 /**
  * 单元测试
  */
 public class ApiTest {
+
+    @Test
+    public void query(){
+
+    }
+
+    @Test
+    public void answer(){
+
+    }
+
 }
